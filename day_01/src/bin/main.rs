c9ed25427ff3786fbd589d7a82104b2174fed6e0
harryalaw <harryalaw@gmail.com>
2022-12-03 07:54:46 +0000

    Add day 1

---
+++ day_01/src/bin/main.rs
@@ -0,0 +1,42 @@
+fn main() {
+    do_part1();
+    println!();
+    do_part2();
+}
+
+fn do_part1() {
+    println!("Test 1: {}", part1(include_str!("../../test_day01.txt")));
+    println!("Part 1: {}", part1(include_str!("../../input_day01.txt")));
+}
+
+fn do_part2() {
+    println!("Test 2: {}", part2(include_str!("../../test_day01.txt")));
+    println!("Part 2: {}", part2(include_str!("../../input_day01.txt")));
+}
+
+fn part1(input: &str) -> i32 {
+    let output = input
+        .split("\n\n")
+        .map(|line| {
+            line.lines()
+                .map(|num| num.parse::<i32>().unwrap())
+                .sum::<i32>()
+        })
+        .max();
+
+    return output.unwrap();
+}
+
+fn part2(input: &str) -> i32 {
+    let mut output: Vec<i32> = input
+        .split("\n\n")
+        .map(|line| {
+            line.lines()
+                .map(|num| num.parse::<i32>().unwrap())
+                .sum::<i32>()
+        })
+        .collect();
+
+    output.sort_by(|a, b| b.cmp(a));
+    return output.iter().take(3).sum::<i32>();
+}
