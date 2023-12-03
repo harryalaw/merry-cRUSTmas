8a1076f170c7c826dd5606511ea6017485cf72bf
harryalaw <harryalaw@gmail.com>
2023-12-03 19:46:42 +0000

    Make day1 p1 faster

--- 2023/day-01/src/part1.rs
+++ 2023/day-01/src/part1.rs
@@ -1,83 +1,22 @@
-use std::str::FromStr;
-
 #[tracing::instrument]
-pub fn process(input: &str) -> i32 {
-    return input
-        .lines()
-        .map(|line| line.parse::<Digits>().unwrap().value)
-        .sum();
+pub fn process(input: &str) -> u32 {
+    return input.lines().map(parse_words).sum();
 }
 
-struct Digits {
-    value: i32,
-}
+fn parse_words(s: &str) -> u32 {
+    let first_idx = s
+        .find(|x: char| x.is_digit(10))
+        .expect("should have one digit");
+    let last_idx = s.rfind(|x: char| x.is_digit(10)).unwrap_or(first_idx);
 
-const NUMBERS: &'static [&'static str] = &["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    let first_number: u32 = s[first_idx..=first_idx]
+        .parse()
+        .expect("Failed to parse first digit");
+    let last_number: u32 = s[last_idx..=last_idx]
+        .parse()
+        .expect("Failed to parse last digit");
 
-impl FromStr for Digits {
-    type Err = ();
-
-    fn from_str(s: &str) -> Result<Digits, Self::Err> {
-        let value = parse_words(s, NUMBERS);
-        return Ok(Digits { value });
-    }
-}
-
-struct Numbers<'a> {
-    first: (usize, &'a str),
-    last: (usize, &'a str),
-}
-
-impl Numbers<'_> {
-    fn value(&self) -> i32 {
-        let first_number = to_number(self.first.1);
-        let last_number = to_number(self.last.1);
-
-        first_number * 10 + last_number
-    }
-}
-
-fn parse_words(s: &str, valid_words: &[&'static str]) -> i32 {
-    valid_words
-        .iter()
-        .flat_map(|word| (s.match_indices(*word).into_iter()))
-        .fold(
-            Numbers {
-                first: (usize::MAX, ""),
-                last: (0, ""),
-            },
-            |acc, x| match (x.0 < acc.first.0, x.0 < acc.last.0) {
-                (true, true) => Numbers {
-                    first: x,
-                    last: acc.last,
-                },
-                (true, false) => Numbers { first: x, last: x },
-                (false, true) => Numbers {
-                    first: acc.first,
-                    last: acc.last,
-                },
-                (false, false) => Numbers {
-                    first: acc.first,
-                    last: x,
-                },
-            },
-        )
-        .value()
-}
-
-fn to_number(s: &str) -> i32 {
-    match s {
-        "1" | "one" => 1,
-        "2" | "two" => 2,
-        "3" | "three" => 3,
-        "4" | "four" => 4,
-        "5" | "five" => 5,
-        "6" | "six" => 6,
-        "7" | "seven" => 7,
-        "8" | "eight" => 8,
-        "9" | "nine" => 9,
-        _ => 0,
-    }
+    first_number * 10 + last_number
 }
 
 #[cfg(test)]
