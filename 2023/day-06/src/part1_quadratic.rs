8b083fc50d1e736d36b668899eaa4f46823f80c9
harryalaw <harryalaw@gmail.com>
2023-12-06 13:54:52 +0000

    Fix parsing logic

--- 2023/day-06/src/part1_quadratic.rs
+++ 2023/day-06/src/part1_quadratic.rs
@@ -1,57 +1,61 @@
 #[tracing::instrument]
-pub fn process(input: &str) -> f32 {
+pub fn process(input: &str) -> f64 {
     let races = parse_races(input);
 
     races
         .iter()
         .map(|race| run_race(*race))
-        .fold(1_f32, |acc, curr| acc * curr)
+        .fold(1_f64, |acc, curr| acc * curr)
 }
 
-fn run_race(race: (f32, f32)) -> f32 {
+fn run_race(race: (f64, f64)) -> f64 {
     let total_time = race.0;
     let distance = race.1;
 
-    let discriminant_squared = total_time * total_time - 4_f32 * distance;
+    let discriminant_squared = total_time * total_time - 4_f64 * distance;
     let discriminant = discriminant_squared.sqrt();
 
-    let speed_min = (total_time - discriminant) / 2_f32;
-    let speed_max = (total_time + discriminant) / 2_f32;
+    let speed_min = (total_time - discriminant) / 2_f64;
+    let speed_max = (total_time + discriminant) / 2_f64;
 
-    let speed_min_int = speed_min.ceil();
-    let speed_max_int = bad_floor(speed_max);
+    let speed_min_int = next_int(speed_min);
+    let speed_max_int = prev_int(speed_max);
 
-    speed_max_int - speed_min_int 
+    speed_max_int - speed_min_int + 1_f64
 }
 
-// handle edge case where it all works out
-// so that the top value is 1 more than the largest value so that
-// high - low = number of values that work
-fn bad_floor(num: f32) -> f32 {
+fn next_int(num: f64) -> f64 {
+    if num.ceil() == num {
+        return num + 1.0
+    }
+    num.ceil()
+}
+
+fn prev_int(num: f64) -> f64 {
     if num.floor() == num {
         return num - 1.0;
     }
-    num.floor() + 1.0
+    num.floor() 
 }
 
-fn parse_races(input: &str) -> Vec<(f32, f32)> {
+fn parse_races(input: &str) -> Vec<(f64, f64)> {
     let lines = input.split_once("\n").expect("Unix endings");
 
-    let times: Vec<f32> = lines
+    let times: Vec<f64> = lines
         .0
         .split_once(": ")
         .expect("Starts with a colon")
         .1
         .split_ascii_whitespace()
-        .flat_map(|x| x.trim().parse::<f32>())
+        .flat_map(|x| x.trim().parse::<f64>())
         .collect();
-    let distances: Vec<f32> = lines
+    let distances: Vec<f64> = lines
         .1
         .split_once(": ")
         .expect("Starts with a colon")
         .1
         .split_ascii_whitespace()
-        .flat_map(|x| x.trim().parse::<f32>())
+        .flat_map(|x| x.trim().parse::<f64>())
         .collect();
 
     times
--- 2023/day-06/src/part2_quadratic.rs
+++ 2023/day-06/src/part2_quadratic.rs
@@ -1,40 +1,44 @@
 #[tracing::instrument]
-pub fn process(input: &str) -> f32 {
+pub fn process(input: &str) -> f64 {
     let race = parse_races(input);
 
     run_race(race)
 }
 
-fn run_race(race: (f32, f32)) -> f32 {
+fn run_race(race: (f64, f64)) -> f64 {
     let total_time = race.0;
     let distance = race.1;
 
-    let discriminant_squared = total_time * total_time - 4_f32 * distance;
+    let discriminant_squared = total_time * total_time - 4_f64 * distance;
     let discriminant = discriminant_squared.sqrt();
 
-    let speed_min = (total_time - discriminant) / 2_f32;
-    let speed_max = (total_time + discriminant) / 2_f32;
+    let speed_min = (total_time - discriminant) / 2_f64;
+    let speed_max = (total_time + discriminant) / 2_f64;
 
-    let speed_min_int = speed_min.ceil();
-    let speed_max_int = bad_floor(speed_max);
+    let speed_min_int = next_int(speed_min);
+    let speed_max_int = prev_int(speed_max);
 
-    speed_max_int - speed_min_int 
+    speed_max_int - speed_min_int + 1_f64
 }
 
-// handle edge case where it all works out
-// so that the top value is 1 more than the largest value so that
-// high - low = number of values that work
-fn bad_floor(num: f32) -> f32 {
+fn next_int(num: f64) -> f64 {
+    if num.ceil() == num {
+        return num + 1.0;
+    }
+    num.ceil()
+}
+
+fn prev_int(num: f64) -> f64 {
     if num.floor() == num {
         return num - 1.0;
     }
-    num.floor() + 1.0
+    num.floor()
 }
 
-fn parse_races(input: &str) -> (f32, f32) {
+fn parse_races(input: &str) -> (f64, f64) {
     let lines = input.split_once("\n").expect("Unix endings");
 
-    let time: f32 = lines
+    let time: f64 = lines
         .0
         .split_once(": ")
         .expect("Starts with a colon")
@@ -45,7 +49,7 @@ fn parse_races(input: &str) -> (f32, f32) {
         .parse()
         .expect("It's a number");
 
-    let distance: f32 = lines
+    let distance: f64 = lines
         .1
         .split_once(": ")
         .expect("Starts with a colon")
