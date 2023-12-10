19da2aef772b994cabdfc9e07b09126e6fd7422d
harryalaw <harryalaw@gmail.com>
2023-12-10 19:15:41 +0000

    Part1 rewrite to make coords nicer

--- 2023/day-10/src/part1.rs
+++ 2023/day-10/src/part1.rs
@@ -11,7 +11,7 @@ fn traverse(pipe_grid: PipeGrid, start: Coord) -> usize {
     let mut distance = 0;
 
     let mut curr_coords = vec![start];
-    visited[u_size(start.row)][u_size(start.col)] = true;
+    visited[start.row][start.col] = true;
 
     while !curr_coords.is_empty() {
         distance += 1;
@@ -23,9 +23,9 @@ fn traverse(pipe_grid: PipeGrid, start: Coord) -> usize {
                 .get(&coord.hash())
                 .expect("Should be in map");
             for nbr in nbrs {
-                if !visited[u_size(nbr.row)][u_size(nbr.col)] {
+                if !visited[nbr.row][nbr.col] {
                     next_coords.push(*nbr);
-                    visited[u_size(nbr.row)][u_size(nbr.col)] = true;
+                    visited[nbr.row][nbr.col] = true;
                 }
             }
         }
@@ -34,10 +34,6 @@ fn traverse(pipe_grid: PipeGrid, start: Coord) -> usize {
     }
 
     distance - 1
-}
-
-fn u_size(val: isize) -> usize {
-    val.try_into().unwrap()
 }
 
 struct PipeGrid {
@@ -56,97 +52,74 @@ fn parse_input(input: &str) -> (Coord, PipeGrid) {
         .enumerate()
         .flat_map(|(row, x)| x.chars().enumerate().map(move |(col, x)| (row, col, x)))
         .filter(|(_row, _col, x)| x != &'.')
-        .map(|(row, col, x)| {
-            (
-                x,
-                Coord {
-                    row: row.try_into().unwrap(),
-                    col: col.try_into().unwrap(),
-                },
-            )
-        })
+        .map(|(row, col, x)| (x, Coord::new(row, col)))
         .collect();
 
     let mut pipe_map = HashMap::new();
 
     pipes.iter().for_each(|(symbol, coord)| {
         let hash_value = coord.hash();
+        let mut vec = Vec::with_capacity(2);
         let neighbours = match *symbol {
             //is a vertical pipe connecting north and south.
-            '|' => vec![
-                Coord {
-                    row: coord.row - 1,
-                    col: coord.col,
-                },
-                Coord {
-                    row: coord.row + 1,
-                    col: coord.col,
-                },
-            ],
-            //is a horizontal pipe connecting east and west.
-            '-' => vec![
-                Coord {
-                    row: coord.row,
-                    col: coord.col - 1,
-                },
-                Coord {
-                    row: coord.row,
-                    col: coord.col + 1,
-                },
-            ],
+            '|' => {
+                if coord.row > 0 {
+                    vec.push(coord.neighbour(Direction::North));
+                }
+                if coord.row < height - 1 {
+                    vec.push(coord.neighbour(Direction::South));
+                }
+                vec
+            }
+            '-' => {
+                if coord.col > 0 {
+                    vec.push(coord.neighbour(Direction::West));
+                }
+                if coord.col < width - 1 {
+                    vec.push(coord.neighbour(Direction::East));
+                }
+                vec
+            }
             //is a 90-degree bend connecting north and east.
-            'L' => vec![
-                Coord {
-                    row: coord.row - 1,
-                    col: coord.col,
-                },
-                Coord {
-                    row: coord.row,
-                    col: coord.col + 1,
-                },
-            ],
+            'L' => {
+                if coord.row > 0 {
+                    vec.push(coord.neighbour(Direction::North));
+                }
+                if coord.col < width - 1 {
+                    vec.push(coord.neighbour(Direction::East));
+                }
+                vec
+            }
             //is a 90-degree bend connecting north and west.
             'J' => {
                 if coord.row > 0 {
-                    vec![
-                        Coord {
-                            row: coord.row - 1,
-                            col: coord.col,
-                        },
-                        Coord {
-                            row: coord.row,
-                            col: coord.col - 1,
-                        },
-                    ]
-                } else {
-                    vec![Coord {
-                        row: coord.row,
-                        col: coord.col - 1,
-                    }]
+                    vec.push(coord.neighbour(Direction::North));
                 }
+                if coord.col > 0 {
+                    vec.push(coord.neighbour(Direction::West));
+                }
+                vec
             }
             //is a 90-degree bend connecting south and west.
-            '7' => vec![
-                Coord {
-                    row: coord.row + 1,
-                    col: coord.col,
-                },
-                Coord {
-                    row: coord.row,
-                    col: coord.col - 1,
-                },
-            ],
+            '7' => {
+                if coord.col > 0 {
+                    vec.push(coord.neighbour(Direction::West));
+                }
+                if coord.row < height - 1 {
+                    vec.push(coord.neighbour(Direction::South));
+                }
+                vec
+            }
             //is a 90-degree bend connecting south and east.
-            'F' => vec![
-                Coord {
-                    row: coord.row + 1,
-                    col: coord.col,
-                },
-                Coord {
-                    row: coord.row,
-                    col: coord.col + 1,
-                },
-            ],
+            'F' => {
+                if coord.col < width - 1 {
+                    vec.push(coord.neighbour(Direction::East));
+                }
+                if coord.row < height - 1 {
+                    vec.push(coord.neighbour(Direction::South));
+                }
+                vec
+            }
             //is the starting position of the animal;
             //there is a pipe on this tile, but your sketch doesn't show what shape the pipe has.
             'S' => {
@@ -154,72 +127,59 @@ fn parse_input(input: &str) -> (Coord, PipeGrid) {
                     row: coord.row,
                     col: coord.col,
                 };
-                let mut start_nbrs = Vec::new();
                 if coord.row > 0 {
                     if let Some(north) = input
                         .lines()
-                        .nth((coord.row - 1).try_into().unwrap())
+                        .nth(coord.row - 1)
                         .unwrap()
                         .chars()
-                        .nth(coord.col.try_into().unwrap())
+                        .nth(coord.col)
                     {
                         if north == '|' || north == 'F' || north == '7' {
-                            start_nbrs.push(Coord {
-                                row: coord.row - 1,
-                                col: coord.col,
-                            });
+                            vec.push(coord.neighbour(Direction::North));
                         }
                     }
                 }
-                if coord.row < (height - 1).try_into().unwrap() {
+                if coord.row < (height - 1) {
                     if let Some(south) = input
                         .lines()
-                        .nth((coord.row + 1).try_into().unwrap())
+                        .nth(coord.row + 1)
                         .unwrap()
                         .chars()
-                        .nth(coord.col.try_into().unwrap())
+                        .nth(coord.col)
                     {
                         if south == '|' || south == 'J' || south == 'L' {
-                            start_nbrs.push(Coord {
-                                row: coord.row + 1,
-                                col: coord.col,
-                            });
+                            vec.push(coord.neighbour(Direction::South));
                         }
                     }
                 }
-                if coord.col < (width - 1).try_into().unwrap() {
+                if coord.col < (width - 1) {
                     if let Some(east) = input
                         .lines()
-                        .nth(coord.row.try_into().unwrap())
+                        .nth(coord.row)
                         .unwrap()
                         .chars()
-                        .nth((coord.col + 1).try_into().unwrap())
+                        .nth(coord.col + 1)
                     {
                         if east == '-' || east == 'J' || east == '7' {
-                            start_nbrs.push(Coord {
-                                row: coord.row,
-                                col: coord.col + 1,
-                            });
+                            vec.push(coord.neighbour(Direction::East));
                         }
                     }
                 }
                 if coord.col > 0 {
                     if let Some(west) = input
                         .lines()
-                        .nth(coord.row.try_into().unwrap())
+                        .nth(coord.row)
                         .unwrap()
                         .chars()
-                        .nth((coord.col - 1).try_into().unwrap())
+                        .nth(coord.col - 1)
                     {
                         if west == '-' || west == 'L' || west == 'F' {
-                            start_nbrs.push(Coord {
-                                row: coord.row,
-                                col: coord.col - 1,
-                            });
+                            vec.push(coord.neighbour(Direction::West))
                         }
                     }
                 }
-                start_nbrs
+                vec
             }
             _ => panic!("Unexpected symbol detected {}", symbol),
         };
@@ -237,15 +197,35 @@ fn parse_input(input: &str) -> (Coord, PipeGrid) {
     )
 }
 
+enum Direction {
+    North,
+    South,
+    East,
+    West,
+}
+
 #[derive(Debug, Copy, Clone)]
 struct Coord {
-    row: isize,
-    col: isize,
+    row: usize,
+    col: usize,
 }
 
 impl Coord {
     fn hash(&self) -> usize {
-        (self.row * 10000 + self.col).try_into().unwrap()
+        self.row * 10000 + self.col
+    }
+
+    fn new(row: usize, col: usize) -> Coord {
+        Coord { row, col }
+    }
+
+    fn neighbour(&self, dir: Direction) -> Coord {
+        match dir {
+            Direction::North => Coord::new(self.row - 1, self.col),
+            Direction::South => Coord::new(self.row + 1, self.col),
+            Direction::East => Coord::new(self.row, self.col + 1),
+            Direction::West => Coord::new(self.row, self.col - 1),
+        }
     }
 }
 
