be2bfd4fd56366695fb4f8930a5854cd5f54e87b
harryalaw <harryalaw@gmail.com>
2023-12-11 12:10:53 +0000

    Simplify the summing

--- 2023/day-11/src/part1.rs
+++ 2023/day-11/src/part1.rs
@@ -1,45 +1,25 @@
 #[tracing::instrument]
 pub fn process(input: &str) -> usize {
-    let (galaxies, empty_rows, empty_cols) = parse_input(input);
+    let (galaxy_rows, galaxy_cols) = parse_input(input);
 
+    distances(&galaxy_rows) + distances(&galaxy_cols)
+}
+
+fn distances(coord: &[usize]) -> usize {
     let mut total = 0;
-    for i in 0..galaxies.len() {
-        for j in i + 1..galaxies.len() {
-            let source = galaxies[i];
-            let target = galaxies[j];
-            total += distance(source, target, &empty_rows, &empty_cols);
+    for i in 0..coord.len() {
+        for j in i+1..coord.len() {
+            total += coord[j] - coord[i];
         }
     }
-
     total
 }
 
-fn distance(
-    source: (usize, usize),
-    target: (usize, usize),
-    empty_rows: &[usize],
-    empty_cols: &[usize],
-) -> usize {
-    let start_x = source.0.min(target.0);
-    let start_y = source.1.min(target.1);
-
-    let end_x = source.0.max(target.0);
-    let end_y = source.1.max(target.1);
-
-    let rows = empty_rows[end_x]-empty_rows[start_x];
-    let cols = empty_cols[end_y]-empty_cols[start_y];
-
-    let x_distance = end_x - start_x + rows;
-    let y_distance = end_y - start_y + cols;
-
-    x_distance + y_distance
-}
-
-fn parse_input(input: &str) -> (Vec<(usize, usize)>, Vec<usize>, Vec<usize>) {
+fn parse_input(input: &str) -> (Vec<usize>, Vec<usize>) {
     let grid: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
 
-    let mut galaxies = Vec::new();
-    let mut empty_rows = Vec::new();
+    let mut galaxy_rows = Vec::new();
+    let mut galaxy_cols = Vec::new();
 
     let mut empty_row_count = 0;
     for row in 0..grid.len() {
@@ -47,31 +27,29 @@ fn parse_input(input: &str) -> (Vec<(usize, usize)>, Vec<usize>, Vec<usize>) {
         for col in 0..grid[0].len() {
             if grid[row][col] == '#' {
                 row_empty = false;
-                galaxies.push((row, col));
+                galaxy_rows.push(row + empty_row_count);
             }
         }
         if row_empty {
             empty_row_count += 1;
         }
-        empty_rows.push(empty_row_count);
     }
 
-    let mut empty_cols = Vec::new();
     let mut empty_col_count = 0;
     for col in 0..grid[0].len() {
         let mut col_empty = true;
         for row in &grid {
             if row[col] == '#' {
+                galaxy_cols.push(col + empty_col_count);
                 col_empty = false;
             }
         }
         if col_empty {
             empty_col_count += 1;
         }
-        empty_cols.push(empty_col_count);
     }
 
-    (galaxies, empty_rows, empty_cols)
+    (galaxy_rows, galaxy_cols)
 }
 
 #[cfg(test)]
--- 2023/day-11/src/part2.rs
+++ 2023/day-11/src/part2.rs
@@ -4,78 +4,56 @@ pub fn process(input: &str) -> usize {
 }
 
 fn solve(input: &str, scaling_factor: usize) -> usize {
-    let (galaxies, empty_rows, empty_cols) = parse_input(input);
+    let (galaxy_rows, galaxy_cols) = parse_input(input, scaling_factor);
 
+    distances(&galaxy_rows) + distances(&galaxy_cols)
+}
+
+fn distances(coord: &[usize]) -> usize {
     let mut total = 0;
-    for i in 0..galaxies.len() {
-        for j in i + 1..galaxies.len() {
-            let source = galaxies[i];
-            let target = galaxies[j];
-            total += distance(source, target, &empty_rows, &empty_cols, scaling_factor);
+    for i in 0..coord.len() {
+        for j in i+1..coord.len() {
+            total += coord[j] - coord[i];
         }
     }
-
     total
 }
 
-fn distance(
-    source: (usize, usize),
-    target: (usize, usize),
-    empty_rows: &[usize],
-    empty_cols: &[usize],
-    scaling_factor: usize,
-) -> usize {
-    let start_x = source.0.min(target.0);
-    let start_y = source.1.min(target.1);
-
-    let end_x = source.0.max(target.0);
-    let end_y = source.1.max(target.1);
-
-    let rows = empty_rows[end_x] - empty_rows[start_x];
-    let cols = empty_cols[end_y] - empty_cols[start_y];
-
-    let x_distance = end_x - start_x + (scaling_factor - 1) * rows;
-    let y_distance = end_y - start_y + (scaling_factor - 1) * cols;
-
-    x_distance + y_distance
-}
-
-fn parse_input(input: &str) -> (Vec<(usize, usize)>, Vec<usize>, Vec<usize>) {
+fn parse_input(input: &str, scaling_factor: usize) -> (Vec<usize>, Vec<usize>) {
     let grid: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
 
-    let mut galaxies = Vec::new();
-    let mut empty_rows = Vec::with_capacity(grid.len());
+    let mut galaxy_rows = Vec::new();
+    let mut galaxy_cols = Vec::new();
+
     let mut empty_row_count = 0;
     for row in 0..grid.len() {
         let mut row_empty = true;
         for col in 0..grid[0].len() {
             if grid[row][col] == '#' {
                 row_empty = false;
-                galaxies.push((row, col));
+                galaxy_rows.push(row + (scaling_factor - 1) *empty_row_count);
             }
         }
         if row_empty {
             empty_row_count += 1;
         }
-        empty_rows.push(empty_row_count);
     }
 
-    let mut empty_cols = Vec::with_capacity(grid[0].len());
     let mut empty_col_count = 0;
     for col in 0..grid[0].len() {
         let mut col_empty = true;
         for row in &grid {
             if row[col] == '#' {
+                galaxy_cols.push(col + (scaling_factor - 1) *empty_col_count);
                 col_empty = false;
             }
         }
         if col_empty {
             empty_col_count += 1;
         }
-        empty_cols.push(empty_col_count);
     }
 
-    (galaxies, empty_rows, empty_cols)
+    (galaxy_rows, galaxy_cols)
 }
 
 #[cfg(test)]
