64d5581333185c9ab4caaea6cbbad8234625205f
harryalaw <harryalaw@gmail.com>
2024-03-11 15:09:57 +0000

    Generate rest of days

---
+++ 2023/day-23/src/bin/part2.rs
@@ -0,0 +1,9 @@
+use day_23::part2::process;
+
+#[tracing::instrument]
+fn main() -> Result<(), ()> {
+    let file = include_str!("../../input.txt");
+    let result = process(file);
+    println!("{}", result);
+    Ok(())
+}
